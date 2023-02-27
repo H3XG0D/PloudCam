a57956b1d749dc568f07b7b17d22e484ec7a7a4b
H3XG0D <99123233+H3XG0D@users.noreply.github.com>
2023-02-27 18:41:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 from traffic_counter import TrafficCounter
 
 def CLI():
-    #Define default values here to make documentation self-updating
+    # Определяю здесь значения по умолчанию, чтобы сделать документацию самообновляющейся
     minArea_default       = 200
     direction_default     = ['H','0.5']
     numCount_default      = 10
@@ -10,24 +10,24 @@ def CLI():
     videoParams_default   = ['mjpg','avi']
     startingFrame_default = 10
 
-    parser = argparse.ArgumentParser(description='Finds the contours on a video file')          #creates a parser object
-    parser.add_argument('-p','--path',type=str,help="""A video filename or path.
-    Works better with .avi files.
-    If no path or name is provided, the camera will be used instead.""")        #instead of using metavar='--path', just type '--path'. For some reason the metavar argument was causing problems
-    parser.add_argument('-a','--minArea',type=int,help=f'The minimum area (in pixels) to draw a bounding box (default is {minArea_default})',
+    parser = argparse.ArgumentParser(description='Находит контуры на видеофайле')          # Создал объект синтаксического анализатора
+    parser.add_argument('-p', '--path', type=str, help="""Название видео или путь до него.
+    Работает лучше с .avi файлами.
+    Если путь или имя не указаны, вместо них будет использована камера.""")        # Вместо использования меты='--path', просто ввожу '--path'. По какой-то причине аргумент меты вызывал проблемы.
+    parser.add_argument('-a', '--minArea', type=int, help=f'Минимальная площадь (в пикселях) для рисования ограничительной рамки (обычно это: {minArea_default})',
                         default=minArea_default)
-    parser.add_argument('-d','--direction', type=str,default=direction_default,nargs=2,help=f"""A character: H or V
-    representing the orientation of the count line. H is horizontal, V is vertical.
-    If not provided, the default is {direction_default[0]},{direction_default[1]}. The second parameter
-    is a float number from 0 to 1 indicating the place at which the
-    line should be drawn.""")
-    parser.add_argument('-n','--numCount',type=int,default=numCount_default,help=f"""The number of contours to be detected by the program (default is {numCount_default}).""")
-    parser.add_argument('-w','--webcam',type=int,nargs='+',help="""Allows the user to specify which to use as the video source""")
-    parser.add_argument('--rgb',action='store_true',help="Boolean flag to use rbg colors.")
-    parser.add_argument('-vo','--video_out',type=str,default="",help="Provide a video filename to output")
-    parser.add_argument('-vw','--video_width',type=int,default=videoWidth_default,help=f"Videos will be resized to this width (default is {videoWidth_default}). Height will be computed automatically to preserve aspect ratio")
-    parser.add_argument('-vp','--video_params',type=str,default=videoParams_default,nargs=2,help=f"Provide video codec and extension (in that order) for the output video. Example: `--video_params mjpg avi`. Default values are {videoParams_default[0]} {videoParams_default[1]}")
-    parser.add_argument('-sf','--starting_frame',type=int,default=startingFrame_default,help=f"Select the starting frame for video analysis (default is {startingFrame_default}). All frames before that will still be used for the background average")
+    parser.add_argument('-d', '--direction', type=str, default=direction_default, nargs=2, help=f"""Символ: H или V
+    обозначающий ориентацию счетной линии. H - горизонтальная, V - вертикальная.
+    Если не указано, то по умолчанию {direction_default[0]}, {direction_default[1]}. Второй параметр
+    является плавающим числом от 0 до 1, указывающим место, в котором
+    должна быть проведена линия.""")
+    parser.add_argument('-n', '--numCount', type=int, default=numCount_default,help=f"""Количество контуров, которые должны быть обнаружены программой (обычно это: {numCount_default}).""")
+    parser.add_argument('-w', '--webcam', type=int, nargs='+',help="""Позволяет пользователю указать, что использовать в качестве источника видеосигнала""")
+    parser.add_argument('--rgb', action='store_true', help="Булевский флаг для использования цветов rbg.")
+    parser.add_argument('-vo', '--video_out', type=str, default="",help="Укажите имя видеофайла для вывода")
+    parser.add_argument('-vw', '--video_width', type=int, default=videoWidth_default,help=f"Размер видео будет изменен до этой ширины (обычно это: {videoWidth_default}). Высота будет вычисляться автоматически, чтобы сохранить соотношение сторон")
+    parser.add_argument('-vp', '--video_params', type=str, default=videoParams_default,nargs=2,help=f"Предоставьте видеокодек и расширение (в таком порядке) для выходного видео. Например: `--video_params mjpg avi`. Обычные значения это: {videoParams_default[0]} {videoParams_default[1]}")
+    parser.add_argument('-sf', '--starting_frame', type=int, default=startingFrame_default,help=f"Выберите начальный кадр для анализа видео (обычно это: {startingFrame_default}). Все кадры до этого будут по-прежнему использоваться для среднего фона")
     args = parser.parse_args()
     return args
 
@@ -36,8 +36,8 @@ def make_video_params_dict(video_params):
     extension = video_params[1]
     
     params_dict = {
-        'codec'    :codec,
-        'extension':extension,
+        'codec'    : codec,
+        'extension': extension,
     }
     return params_dict
 
